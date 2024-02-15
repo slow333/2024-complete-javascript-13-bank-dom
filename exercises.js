a773146b0fd78ab16b5284af8975cac5091aedd2
self esteem <selfesteem@gmail.com>
2024-02-15 14:32:42 +0900

    in the meddle

---
+++ exercises.js
@@ -0,0 +1,65 @@
+'use strict';
+console.log('==== getComputedStyle and getAttribute ====')
+// message.style.backgroundColor = '#373723';
+// message.style.width = '107%';
+// message.style.height =
+//   parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+// document.documentElement.style
+//   .setProperty('--color-primary', 'orangered');
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.src)
+//
+// //getAttribute 하면 사용자 지정한 값 자체를 가지고옮.
+// const nonStandard = logo.getAttribute('madeBy');
+// console.log(nonStandard);
+// // setAttribute 하면 사용자 지정 값을 지정할 수 있음
+// logo.setAttribute('company', 'ai automation')
+// console.log(logo.getAttribute('company'))
+// console.log(logo.getAttribute('src'))
+//
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href)
+// console.log(link.getAttribute('href'))
+//
+// // element 속성에 data-name-description 하면 dataset으로 추출 가능
+// // 추출은 dataset.camelCase
+// const version = logo.dataset.versionNumber;
+// classes  .classList.add('a') remove('c') toggle('d') contain('e')
+// logo.className = 'f' 하면 전체를 지우고 다시 설정함.*/
+// console.log('===========================');
+// const h1 = document.querySelector('h1');
+// // even 한번만 발생하게 하는 방법
+// const h1MouseEnter = function (e) {
+//   alert('mouse enter happened ~~ 🖱️');
+//   // h1.removeEventListener('mouseenter', h1MouseEnter)
+// };
+//
+// h1.addEventListener('mouseenter', h1MouseEnter);
+// setTimeout(() =>
+//   h1.removeEventListener('mouseenter', h1MouseEnter)
+//   , 4000);
+console.log('=============== event bubbling and capturing ===================');
+// parent node에 적용된 이벤트도 같이 적용됨
+/*const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1));
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+document.querySelector('.nav__link')
+  .addEventListener('click', function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log(e.target);
+    console.log(e.currentTarget)
+    // e.stopPropagation();
+  });
+document.querySelector('.nav__links')
+  .addEventListener('click', function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log(e.target);
+    console.log(e.currentTarget)
+  });
+document.querySelector('.nav')
+  .addEventListener('click', function (e) {
+      this.style.backgroundColor = randomColor();
+      console.log(e.target);
+      console.log(e.currentTarget)
+    }
+  );*/
--- script.js
+++ script.js
@@ -8,7 +8,12 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +23,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(modal =>
+  modal.addEventListener('click', openModal))
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +34,87 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+//////////////////////////////////
+// smooth scroll => learn more ↓  
+btnScrollTo.addEventListener('click', function (e) {
+  // e.preventDefault();
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log('s1 => ', s1coords);
+  // console.log('btn scroll to => ', e.target.getBoundingClientRect())
+  // const xPositionFromScreenTopToPage = window.scrollX;
+  // const yPositionFromScreenTopToPage = window.scrollY;
+  // console.log(xPositionFromScreenTopToPage, yPositionFromScreenTopToPage)
+  // console.log('height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth) // 보이는 화면의 크기
+  // window.scrollTo(
+  //   s1coords.x + xPositionFromScreenTopToPage,
+  //   s1coords.y + yPositionFromScreenTopToPage)
+  // window.scroll({
+  //   top: s1coords.top + yPositionFromScreenTopToPage,
+  //   left: s1coords.left + xPositionFromScreenTopToPage,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({behavior: 'smooth'});
+  document.querySelectorAll('.features__img')
+    .forEach(el => el.classList.remove('lazy-img'));
+});
+// smooth : 윈도우 설정이 성능최적화로 되어 있으면 적용안됨. 쩝
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth'})
+//   })
+// });
+
+document.querySelector('.nav__links').addEventListener
+('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id)
+      .scrollIntoView({behavior: 'smooth'});
+    document.querySelectorAll('.features__img')
+      .forEach(el => el.classList.remove('lazy-img'));
+  }
+});
+document.querySelector('.operations__tab-container').addEventListener
+('click', function (e) {
+  e.preventDefault();
+  const tabId = e.target.getAttribute('data-tab')
+  if (e.target.classList.contains('operations__tab')) {
+    document.querySelectorAll('.operations__content')
+      .forEach(el => {
+        el.classList.remove('operations__content--active');
+      })
+    document.querySelectorAll('.operations__tab')
+      .forEach(el => {
+        el.classList.remove('operations__tab--active');
+      })
+    document.querySelector(`.operations__content--${tabId}`)
+      .classList.add('operations__content--active');
+    document.querySelector(`.operations__tab--${tabId}`)
+      .classList.add('operations__tab--active');
+  }
+})
+
+
+/////////////////////////////////
+// message insert ..
+const message = document.createElement('div');
+message.classList.add('cookie-message')
+message.innerHTML = '현재 홈페이지 구축 중입니다.' +
+  '<button class="btn--close-cookie">🎉 OK 🎉 </button>'
+header.prepend(message);
+// header.after(message); // 관련 내용 밖에 붙임.
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function (ev) {
+    ev.preventDefault();
+    // message.remove();
+    message.parentNode.removeChild(message);
+  });
+setTimeout(() =>
+    message.parentNode.removeChild(message)
+  , 2000)
